4dd7e12d7b8de3b4b4155ce9413a8e784d965491
leon <gzapata9804@gmail..com>
2025-03-17 21:43:32 -0600

    funciones basicas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/saludo', function (Request $request) {
+    return response()->json(['imagen' => 'http://srv743319.hstgr.cloud:8000/static/visualizacion_usuario_13.png']);
 });
