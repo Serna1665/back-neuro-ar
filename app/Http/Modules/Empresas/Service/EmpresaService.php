a8cd047c2e90f58a35457cd1e447fe6b058304e7
leon <gzapata9804@gmail..com>
2025-03-26 22:21:39 -0600

    gestor de empresas

---
+++ app/Http/Modules/Empresas/Service/EmpresaService.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Modules\Empresas\Service;
+
+use App\Services\BaseService;
+
+class EmpresaService extends BaseService
+{
+    // Aquí puedes agregar métodos específicos para EmpresaService
+}
