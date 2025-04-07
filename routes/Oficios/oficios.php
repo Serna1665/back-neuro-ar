607db7808eeb3408ef9b63984d6ea41b670d4851
leon <gzapata9804@gmail..com>
2025-04-07 11:01:48 -0600

    cambios en rutas para habilitacion

--- routes/Oficios/oficios.php
+++ routes/Oficios/oficios.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Modules\Oficios\Controllers\OficioController;
 
+Route::get('/oficios/buscar', [OficioController::class, 'buscar']);
+
 Route::get('/oficios/listar', [OficioController::class, 'index']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/oficios/crear-oficio', [OficioController::class, 'crearOficios']);
     Route::put('/oficios/actualizar-oficio/{id}', [OficioController::class, 'actualizarOficio']);
     Route::delete('/oficios/eliminar-oficio/{id}', [OficioController::class, 'eliminarOficio']);
-    Route::get('/oficios/buscar', [OficioController::class, 'buscar']);
 });
